47c3b9e09162c655f100fb106baaf99e8c7bb797
crispysodium <christinena.316@gmail.com>
2020-04-08 04:04:09 -0700

    added logic to controller file

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,3 +1,31 @@
 const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
 
-const db = require("../models/burger.js");
+router.get("/", (req, res) => {
+    burger.all(function(data) {
+        hbsObj = {
+            burger: data
+        };
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    });
+});
+
+router.post("/api/burger", (req, res) => {
+    burger.insertOne(cols, vals, (res) => {
+        res.json({ id: res.insertId });
+    });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+    burger.updateOne({ burger }, condition, (res) => {
+        if(res.changedRows === 0) {
+            res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
