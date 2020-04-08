aed80f5d9a635ea91e90bbc9e85bafc66b77706a
crispysodium <christinena.316@gmail.com>
2020-04-08 04:04:33 -0700

    added boiler plate code to connect to handlebars and the server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+const exhbs = require("express-handlebars");
+
+app.engine("handlebars", exhbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`App listening on localhost: ${PORT}`);
+})
