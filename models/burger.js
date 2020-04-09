0c0de69156471cec769a86b8f5cd459ea326f67b
crispysodium <christinena.316@gmail.com>
2020-04-09 01:16:58 -0700

    fixed orm.all to orm.selectAll

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    all: function(callback) {
-        orm.all(tableName, (res) => {
+    selectAll: function(callback) {
+        orm.selectAll("burgers", (res) => {
             callback(res);
         });
     },
     insertOne: function(cols, vals, callback) {
-        orm.insertOne(tableName, cols, vals, (res) => {
+        orm.insertOne("burgers", cols, vals, (res) => {
             callback(res);
         });
     },
     updateOne: function(objColVals, condition, callback) {
-        orm.updateOne(tableName, objColVals, condition, (res) => {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
             callback(res);
         });
     }
