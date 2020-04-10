aa3f4dc9ce462281c1c7d339e2af0047b92e4e4b
crispysodium <christinena.316@gmail.com>
2020-04-10 00:28:56 -0700

    updated request methods

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,14 +14,17 @@ router.get("/", (req, res) => {
 
 router.post("/api/burger", (req, res) => {
     burger.insertOne("burger_name", req.body.name, (inserted) => {
-        console.log("Success! Burger " + inserted + " added");
-        res.json({ id: res.insertId });
+        if (inserted.affectedRows === 0) {
+            res.status(404).end();
+        }
+        res.status(200).end();
     });
 });
 
 router.put("/api/burger/:id", (req, res) => {
     const condition = `id = ${req.params.id}`;
-    burger.updateOne({ burger }, condition, (updated) => {
+    //CHECK THIS
+    burger.updateOne(req.body.devoured, condition, (updated) => {
         if(updated.changedRows === 0) {
             res.status(404).end();
         }
